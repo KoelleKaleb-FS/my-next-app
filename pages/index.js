a8c1cdf24dc498f9f8477a75c08e9ebefc3cde46
Kaleb Koelle <kkoelle@student.fullsail.edu>
2024-09-07 19:45:02 -0400

    Manually set up Next.js project

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+export default function Home() {
+    return (
+      <div>
+        <h1>Welcome to My Manually Created Next.js Site!</h1>
+        <p>This site was built without `npx create-next-app`!</p>
+      </div>
+    );
+  }
+  
